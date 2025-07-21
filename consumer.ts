a50d9e316fa0b055340b160c316607bf19c6652b
usorfaitheloho <usorfaitheloho@gmail.com>
2025-07-21 23:18:22 +0300

    Connect a consumer

---
+++ consumer.ts
@@ -0,0 +1,20 @@
+const Kafka = require("node-rdkafka");
+
+const TOPIC_NAME = "testing";
+
+const stream = new Kafka.createReadStream(
+  {
+    "metadata.broker.list": "practise1-project-kafka-dev-usorfaitheloho-2f87.j.aivencloud.com:18136",
+    "group.id": "GROUP_ID",
+    "security.protocol": "ssl",
+    "ssl.key.location": "service.key",
+    "ssl.certificate.location": "service.cert",
+    "ssl.ca.location": "ca.pem",
+  },
+  { "auto.offset.reset": "beginning" },
+  { topics: [TOPIC_NAME] }
+);
+
+stream.on("data", (message) => {
+  console.log("Got message using SSL:", message.value.toString());
+});
