c6f3fe85e075a2f392d48697bc30b321fdeb9f4e
usorfaitheloho <usorfaitheloho@gmail.com>
2025-07-22 00:00:46 +0300

    Connect to producer

--- consumer.ts
+++ consumer.ts
@@ -7,9 +7,9 @@ const stream = new Kafka.createReadStream(
     "metadata.broker.list": "practise1-project-kafka-dev-usorfaitheloho-2f87.j.aivencloud.com:18136",
     "group.id": "GROUP_ID",
     "security.protocol": "ssl",
-    "ssl.key.location": "service.key",
-    "ssl.certificate.location": "service.cert",
-    "ssl.ca.location": "ca.pem",
+    "ssl.key.location": "./certificates/service.key",
+    "ssl.certificate.location": "./certificates/service.cert",
+    "ssl.ca.location": "./certificates/ca.pem",
   },
   { "auto.offset.reset": "beginning" },
   { topics: [TOPIC_NAME] }
